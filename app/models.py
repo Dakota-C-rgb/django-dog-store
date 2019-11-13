1c3a5a19792ba1d39355d0f9840403ba80c9949d
Base Camp <basecamp@Bases-MacBook-Pro.local>
2019-11-13 10:15:12 -0600

    Added the other two views so I can use them later

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class DogProduct(models.Model):
@@ -8,3 +9,11 @@ class DogProduct(models.Model):
     price = models.FloatField()
     quantity = models.IntegerField()
 
+class Purchase(models.Model):
+    dog_product = models.ForeignKey(DogProduct, on_delete=models.PROTECT)
+    purcahsed_at = models.DateField(datetime.now())
+
+class DogTag(models.Model):
+    owner_name = models.TextField()
+    dog_name = models.TextField()
+    dog_birthday = models.DateField()
