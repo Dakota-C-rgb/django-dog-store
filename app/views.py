697c6530221a5cd3c327bd69e3aa8ae600d4e356
Base Camp <basecamp@Bases-MacBook-Pro.local>
2019-11-15 10:17:53 -0600

    Created the home view to render the home.html template with all the dog_products.
    Created the dog_product_detail view to render the dog_product_detail template that displays the chosen product's details.
    
    Created the purchase_dog_product view that takes the chosen product and takes one off fo the quantity if it is in stock.
    
    Created the purchase_detail view that renders the purchase_detail template along with the product purchased.
    
    Created the new_dog_tag view that takes the form NewDogTagForm and validates the information provided. If the information is valid then it redirects to the dog_tag_list otherwise it renders the new_dog_tag template with the form.
    
    Cerated the dog_tag_list veiw that renders the dog_tag_list template and displays all of the dog tags.

--- app/views.py
+++ app/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from app.models import DogProduct, Purchase, DogTag
+from django.contrib import messages
+from datetime import datetime
+from app.forms import NewDogTagForm
 # Create your views here.
+
+def home(request):
+    dog_products = DogProduct.objects.all()
+    return render(request, "home.html", {"dog_products": dog_products})
+
+
+def dog_product_detail(request, id):
+    dog_product = DogProduct.objects.get(id=id)
+    return render(request, "dog_product_detail.html", {"dog_product": dog_product})
+
+
+def purchase_dog_product(request, id):
+    dog_product = DogProduct.objects.get(id=id)
+    if dog_product.quantity > 0:
+        messages.success(request, f"Purchased {dog_product.name}")
+        dog_product.quantity -= 1
+        dog_product.save()
+        Purchase.objects.create(dog_product=dog_product, purchased_at=datetime.now())
+        return redirect("purchase_detail", dog_product.id)
+
+    elif dog_product.quantity == 0:
+        messages.error(request, f"{dog_product.name} is out of stock")
+        return redirect("dog_product_detail", dog_product.id)
+
+
+def purchase_detail(request, id):
+    purchase = Purchase.objects.get(id=id)
+    return render(request, "purchase_detail.html", {"purchase":purchase})
+
+
+def new_dog_tag(request):
+    if request.method == "GET":
+        return render(request, "new_dog_tag.html")
+    elif request.method == "POST":
+        form = NewDogTagForm(request.POST)
+        if form.is_valid():
+            owner_name = form.cleaned_data["owner_name"]
+            dog_name = form.cleaned_data["dog_name"]
+            dog_birthday = form.cleaned_data["dog_birthday"]
+            new_dog_tag = DogTag.objects.create(owner_name=owner_name, dog_name=dog_name, dog_birthday=dog_birthday)
+            return redirect("dog_tag_list")
+        if not form.is_valid():
+            return render(request, "new_dog_tag.html", {"form":form})
+
+
+def dog_tag_list(request):
+    dog_tags = DogTag.objects.all()
+    return render(request, "dog_tag_list.html", {"dog_tags":dog_tags})
