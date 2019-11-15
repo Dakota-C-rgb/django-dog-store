1865460c3aaa3abe46344b853d261d550a6c8156
Base Camp <basecamp@Bases-MacBook-Pro.local>
2019-11-15 16:09:26 -0600

    Registered models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from app.models import DogTag, DogProduct
 
 # Register your models here.
+@admin.register(DogTag)
+class DogTagAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(DogProduct)
+class DogProductAdmin(admin.ModelAdmin):
+    pass
