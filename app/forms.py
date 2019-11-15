2f49f3874e0f054da490d4bd6d6429fffe2e8ffb
Base Camp <basecamp@Bases-MacBook-Pro.local>
2019-11-15 16:09:57 -0600

    Added two fields to the from

--- app/forms.py
+++ app/forms.py
@@ -4,3 +4,5 @@ class NewDogTagForm(forms.Form):
     owner_name = forms.CharField(min_length=1)
     dog_name = forms.CharField(min_length=1)
     dog_birthday = forms.DateField()
+    owner_address = forms.CharField(min_length=1)
+    dog_color = forms.CharField(min_length=1)
